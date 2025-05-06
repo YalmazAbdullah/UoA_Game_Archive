b3780a1256437be97ba1f472536f0ab81bdd7cbf
Yalmaz Abdullah <yaserver@NBP-WorkServer.>
2025-05-06 01:56:28 -0600

    cleanup

--- src/types/gameType.tsx
+++ src/types/gameType.tsx
@@ -1,10 +1,13 @@
+/**
+ * Type defnition for Games.
+ */
 export interface GameType {
-    id: number;
-    name: string;
-    course: string;
-    year: number;
-    blurb: string;
-    thumbnail: string;
-    releases: { steam?: string }; // Optional steam link
-  }
+  id: number;
+  name: string;
+  course: string;
+  year: number;
+  blurb: string;
+  thumbnail: string;
+  releases: { steam?: string };
+}
   
